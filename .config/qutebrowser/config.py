38b21a9f29d1d8b8d4c2e06545f4719a69d195d4
Rasa Hosseinzadeh <rasa.hosseinzadeh@gmail.com>
2021-07-08 10:52:41 -0400

    Add qutebrowser configs.

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,2 +1,2 @@
-config.load_autoconfig() #Load the autoconf
-config.bind('M', 'hint links spawn --detach mpv --force-window yes {hint-url}') #Mpv key-binding
+config.load_autoconfig()  # Load the autoconf
+config.bind('M', 'hint links spawn --detach mpv --force-window yes {hint-url}')  # Mpv key-binding
