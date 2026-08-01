41ee2b2e9a0d53e6c7a0de699eba73f7c7671531
Rasa Hosseinzdaeh <rasa.hosseinzadeh@gmail.com>
2026-08-01 16:50:34 -0400

    Update firefox(it has vertical tabs without neding cusotm setting) and arkinfox.

--- .config/firefox/user-overrides.js
+++ .config/firefox/user-overrides.js
@@ -11,13 +11,4 @@ user_pref("ui.systemUsesDarkTheme", 1); // [FF67+] [HIDDEN PREF]
 user_pref("extensions.pocket.enabled", false); // Pocket Account [FF46+]
 user_pref("identity.fxaccounts.enabled", false); // Firefox Accounts & Sync [FF60+] [RESTART]
 user_pref("reader.parse-on-load.enabled", true); // Reader View
-/*
- * These are to get chrome folder for custom themming
- */
-user_pref('toolkit.legacyUserProfileCustomizations.stylesheets', true);
-user_pref('layers.acceleration.force-enabled', true);
-user_pref('gfx.webrender.all', true);
-user_pref('gfx.webrender.enabled', true);
-user_pref('layout.css.backdrop-filter.enabled', true);
-user_pref('svg.context-properties.content.enabled', true);
 
